a00f33c8a27c32697d6110fdb7f26ac6ea552ae7
Satyadeo <satyadeo@outlook.in>
2026-04-12 13:20:06 +0530

    Adding print with default separator

--- Print_Function.py
+++ Print_Function.py
@@ -3,10 +3,16 @@
 print('Welcome to python programming !')
 print("This is fun !")
 
-#Heer is how to print list, tuple, and string together using the default separator
+#Here is how to print list, tuple, and string together using the default separator
 #print(*objects, sep=' ', end='\n', file=None, flush=False)
 lst = [1, 2, 3]
 t = ("A", "B")
 s = "Python"
 print(lst, t, s)
+
+#Here is how to print multiple objects but uses a custom separator to control spacing
+lst = [1, 2, 3]
+t = ("A", "B")
+s = "Python"
+print(lst, t, s, sep=" | ")
 
