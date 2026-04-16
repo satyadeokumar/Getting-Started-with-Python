11937915097558ef946d9289bdece757ce324358
Satyadeo <satyadeo@outlook.in>
2026-04-16 07:58:21 +0530

    Adding print with default separator

---
+++ Arithmetic operations.py
@@ -0,0 +1,13 @@
+# Python has multiple arithmetic operations for calculations
+/*
+
+Operator	Description
++	        Addition
+-	        Subtraction
+*	        Multiplication
+/	        Division (float)
+//	        Floor Division
+%	        Modulus (remainder)
+**	        Exponentiation
+*/
+
