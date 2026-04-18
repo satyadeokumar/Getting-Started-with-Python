7ba6d141b58db0424c8d72e2176ee41e7aff82ee
Satyadeo <satyadeo@outlook.in>
2026-04-18 11:50:37 +0530

    Adding print with default separator

---
+++ Numbers and Variables.py
@@ -0,0 +1,9 @@
+"""
+There are certain rules for Naming Variables in python to use variables effectively.
+
+Variable names can only contain letters, digits and underscores (_)
+A variable name cannot start with a digit
+Variable names are case-sensitive
+Avoid using Python keywords like if, else, for as variable names
+"""
+
