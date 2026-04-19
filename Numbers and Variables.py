c11a8b5ecf8442f1893b951f51ec658e3983b618
Satyadeo <satyadeo@outlook.in>
2026-04-19 20:31:05 +0530

    Count_Digita_Letters code

--- Numbers and Variables.py
+++ Numbers and Variables.py
@@ -31,5 +31,18 @@ word2 = "Test Python"
 length = len(word), len(word2)
 print("Length of 'TestPython' and 'Test Python' are :", length)
 
-# Adding few lines prior to merge
+#Write a program which will find factors of given number and find whether the factor is even or odd.
+
+number = 69
+
+print("The factors of {} are,".format(number))
+
+for i in range(1,number+1):
+    if number % i == 0:
+        print(i)
+        if i%2==0:
+            print(i, "It is an Even Number")
+        else:
+            print(i, "It is an Odd Number")
+
 
