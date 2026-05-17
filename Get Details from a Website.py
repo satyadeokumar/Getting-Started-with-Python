dc1b072dee263c1d716fbaf0e7d8f14e877555d5
Satyadeo <satyadeo@outlook.in>
2026-05-17 12:07:09 +0530

    Get Details from a Website

--- Get Details from a Website.py
+++ Get Details from a Website.py
@@ -2,9 +2,11 @@ import urllib.request
 try:
     with urllib.request.urlopen('https://satyadeo.com/goa-a-place-on-everybodys-list/') as f:
         #with urllib.request.urlopen('https://satyadeo.com/') as f:
+        f2 = urllib.request.urlopen('https://satyadeo.com/goa-a-place-on-everybodys-list/')
         print(f.read().decode('utf-8'))
         print("Header Values are :",f.getheaders())
         print("Requested url is :",f.geturl())
+        print("Contents of the url are  :",f2.read())
 
 except urllib.error.URLError as e:
     print(e.reason)
