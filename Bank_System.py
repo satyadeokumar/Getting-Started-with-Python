9d3946b0bac9b147c12f8fc6fd285e481750f5dc
Satyadeo <88426966+satyadeokumar@users.noreply.github.com>
2026-06-06 21:24:54 +0530

    Get Details from a Website

--- Bank_System.py
+++ Bank_System.py
@@ -1,3 +1,5 @@
+'''5. Design a software for bank system. There should be options like cash withdraw, cash credit and change password. According to user input, the software should
+provide required output.Hint: Use if else statements and functions   '''
 
 
 def withdrawl():
