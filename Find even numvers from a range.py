30ace4b69b63d952f72785472d6fbfa1a05e10d9
Satyadeo <88426966+satyadeokumar@users.noreply.github.com>
2026-06-07 19:39:53 +0530

    refactor: improve logic for checking even digits in range

--- Find even numvers from a range.py
+++ Find even numvers from a range.py
@@ -10,18 +10,21 @@ print("Given range is:", m, "to", n)
 print("The numbers in given range with all digits even are: ")
 
 arr=[]      #Creating an empty list
-for i in range(m,n+1):      #Setting a for loop from m to n
-    num=i           #Taking a variable num to work upon
-    isValid=True    #Setting a boolean variable to True
-    #Extract each digit of num and check if it is even
+for i in range(m, n + 1):      # Setting a for loop from m to n
+    num = abs(i)           # Use absolute value to handle negative numbers
+    isValid = True          # Setting a boolean variable to True
+    # Extract each digit of num and check if it is even
 
-    while(num!=0):
-
-        digit=num%10   #Extracting digit
-        num=num//10
-        if(digit%2!=0):     #Checking if digit is odd
-            isValid=False   #If odd, then isValid is set to False
-            break       #Breaking loop if odd digit found
-    if(isValid):        #If isValid remains True, then all digits are even
+    if num == 0:
         arr.append(i)
-print(arr)      #Printing the list
+        continue
+
+    while num != 0:
+        digit = num % 10   # Extracting digit
+        num //= 10
+        if digit % 2 != 0:     # Checking if digit is odd
+            isValid = False   # If odd, then isValid is set to False
+            break       # Breaking loop if odd digit found
+    if isValid:        # If isValid remains True, then all digits are even
+        arr.append(i)
+print(arr)      # Printing the list
