a3918117f3f5d4c43e39d4f54b6b503e586aff41
Satyadeo <88426966+satyadeokumar@users.noreply.github.com>
2026-06-10 10:15:04 +0530

    Get Details from a Website

--- Get Details from a Website.py
+++ Get Details from a Website.py
@@ -1,15 +1,33 @@
+import re
+import urllib.error
 import urllib.request
+
+url = 'https://satyadeo.com/goa-a-place-on-everybodys-list/'
+
 try:
-    with urllib.request.urlopen('https://satyadeo.com/goa-a-place-on-everybodys-list/') as f:
-        #with urllib.request.urlopen('https://satyadeo.com/') as f:
-        print(f.read().decode('utf-8'))
-        print("Header Values are :",f.getheaders())
-        print("Requested url is :",f.geturl())
-        print("Contents of the url are  :",f.read())
-        content = f.read().decode('utf-8')
-        print("Contents of the url are  :",content)
-       # print("Text Contents of the url are  :",f2.text)
+    with urllib.request.urlopen(url) as response:
+        html = response.read().decode('utf-8')
+
+        print('Requested URL      :', response.geturl())
+        print('Status Code        :', response.getcode())
+        print('Header Values      :', response.getheaders())
+
+        # Print the page text for reference
+        print('\nPage Content Preview:')
+        print(html[:1000])
+
+        # Find image tags and extract their source URLs
+        image_tags = re.findall(r'<img\b[^>]*\bsrc=["\']([^"\']+)["\']', html, flags=re.IGNORECASE)
+
+        print('\nImages found on the page:', len(image_tags))
+        if image_tags:
+            for index, src in enumerate(image_tags, start=1):
+                print(f'  {index}. {src}')
+        else:
+            print('  No image tags were found on this page.')
 
 except urllib.error.URLError as e:
-    print(e.reason)
+    print('URL Error:', e.reason)
+except Exception as e:
+    print('Error:', e)
 
