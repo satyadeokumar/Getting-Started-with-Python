44586666f6ad6bfb9a3f084cf67eea1fc49f770d
Satyadeo <88426966+satyadeokumar@users.noreply.github.com>
2026-07-05 22:53:19 +0530

    feat: add functionality to read and print CSV data from in-memory string

--- Read From a File.py
+++ Read From a File.py
@@ -25,3 +25,20 @@ file = open(r"C:\devhome\Github\Getting-Started-with-Python\ExampleFile.txt", "r
 content = file.read(10)
 print(content)
 file.close()
+
+# Read from a csv file
+import csv
+import io
+
+# Create a CSV sample in memory
+csv_data = """Year,Industry,Value
+2014,Manufacturing,769400
+2014,Manufacturing,48000
+2014,Manufacturing,12
+"""
+csvfile = io.StringIO(csv_data)
+csvreader = csv.reader(csvfile)
+for row in csvreader:
+    print(row)
+
+
