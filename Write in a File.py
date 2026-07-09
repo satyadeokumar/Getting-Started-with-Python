436146a0dd46df1dcd60e0701989beb90893c6a3
Satyadeo <88426966+satyadeokumar@users.noreply.github.com>
2026-07-09 15:45:35 +0530

    fix: correct file name and content in file operations

--- Write in a File.py
+++ Write in a File.py
@@ -1,6 +1,6 @@
-with open("file.txt", "w", encoding="utf-8") as f:
-    f.write("Created using write mode.\n")
-    f.write("Second line.\n")
+with open("Dummyfile.txt", "w", encoding="utf-8") as f:
+    f.write("This file is Created using write mode.\n")
+    f.write("Add Second line.\n")
 
-with open("file.txt", "r", encoding="utf-8") as f:
+with open("Dummyfile.txt", "r", encoding="utf-8") as f:
     print(f.read())
