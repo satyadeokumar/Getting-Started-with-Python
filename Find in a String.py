6690ce3af2ce999377bb771fecc5099455669555
Satyadeo <88426966+satyadeokumar@users.noreply.github.com>
2026-07-18 08:24:23 +0530

    fix: correct variable name and add additional print statements in Find in a String example

--- Find in a String.py
+++ Find in a String.py
@@ -1,5 +1,8 @@
-txt = " This is a python string"
-
-x = txt.find("python")
+Text = " This is a python string"
+x = Text.find("python")
+y = Text.find("is")
+z = Text.find("e", 5)
 
 print(x)
+print(y)
+print(z)
